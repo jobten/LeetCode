b35cd853d44935b9b1b036711ff479fba8261648
mac <mac@macdeMacBook-Pro.local>
2020-07-07 21:23:41 +0800

    update

--- 66.加一.js
+++ 66.加一.js
@@ -9,6 +9,7 @@
 // 1. 小于9，加1
 // 2. 等于9，变成0，前一位加1
 // 3. 等于10，变成1, 前一位加1
+// 4. 注意添加plused标志
 /**
  * @param {number[]} digits
  * @return {number[]}
@@ -22,16 +23,22 @@ var plusOne = function(digits) {
     let i = digits.length - 1
     let plused = false
     while(i > 0) {
-        if(digits[i] < 9) {
-            if (plused) plused = false
-            else digits[i]++
-            break
-        } else if (digits[i] === 9 || digits[i] === 10) {
-            if (plused) plused = false
-            else {
+        if (!plused) {
+            if(digits[i] < 9) {
+                digits[i]++
+                break
+            } else if (digits[i] === 9) {
                 digits[i] = 0
                 digits[i-1]++
                 plused = true
+            }
+        } else {
+            if (digits[i] === 10) {
+                digits[i] = 0
+                digits[i-1]++
+            } else {
+                plused = false
+                break
             }
         }
         i--
